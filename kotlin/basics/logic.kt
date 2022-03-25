152e702afd1fc3b92c6ffc7e27e3815abdf8cc55
Forrest <rainforest92@126.com>
2022-03-25 20:32:23 +0800

    Semantics

---
+++ kotlin/basics/logic.kt
@@ -0,0 +1,6 @@
+fun logic1(param1: String, param2: Int?, param3: String) {
+}
+
+fun main() {
+
+}
---
+++ ts/basics/logic.ts
@@ -0,0 +1,3 @@
+function logic1(param1: string, param2?: number, param3: string = '') {
+    let variable1: string;
+}
